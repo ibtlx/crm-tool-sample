bd45e3ee1d2badd8daab597764e86418948c9620
ibtlx <ibtihal542@gmail.com>
2024-09-04 01:19:22 +0400

    Add files via upload

---
+++ crmtool/src/main/java/com/ibtihal/crmtool/CrmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.ibtihal.crmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmtoolApplication.class, args);
+	}
+
+}
---
+++ crmtool/src/test/java/com/ibtihal/crmtool/CrmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibtihal.crmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
